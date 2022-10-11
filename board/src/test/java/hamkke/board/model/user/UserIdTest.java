96cae267b012c0fc0dee1f62ff08c6eb3ff632d5
cjh8746 <cjh87467@gmail.com>
2022-10-11 16:29:32 +0900

    test : 유저 아이디 테스트코드 작성

---
+++ board/src/test/java/hamkke/board/model/user/UserIdTest.java
@@ -0,0 +1,36 @@
+package hamkke.board.model.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+class UserIdTest {
+
+    @ParameterizedTest
+    @DisplayName("유저의 아이디는 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.")
+    @ValueSource(strings = {"1apple", "_apple", " apple", "apple", "a12345678901234567890", "apple!", "@apple", "apple_hi", "", " "})
+    void validateUserId(final String inputUserId) {
+        //when, then
+        assertThatThrownBy(() -> new UserId(inputUserId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유저의 아이디는 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.");
+    }
+
+    @Test
+    @DisplayName("유저 아이디를 반환하다.")
+    void getValue() {
+        //given
+        UserId apple = new UserId("banana12");
+
+        //when
+        String actual = apple.getValue();
+
+        //then
+        assertThat(actual).isEqualTo("banana12");
+    }
+}
