c6bcb6f2c2f7f7a9d7719ea68d893fd7ae80a472
cjh8746 <cjh87467@gmail.com>
2022-10-12 00:14:39 +0900

    test : Password 테스트 코드 작성

---
+++ board/src/test/java/hamkke/board/model/user/PasswordTest.java
@@ -0,0 +1,35 @@
+package hamkke.board.model.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PasswordTest {
+
+    @ParameterizedTest
+    @DisplayName("비밀번호는 영문(대문자,소문자), 특수문자(!,@,#,$,%,^,&,*,_,-), 숫자를 최소 1개 이상 조합한 8자 이상 18자 이하여야합니다.")
+    @ValueSource(strings = {"apple1!", "0123456789012345a1!", "appleBanana1", "appleBanana!", "apple012345?", "     apple", ""})
+    void validate(final String inputPassword) {
+
+        //when, then
+        assertThatThrownBy(() -> new Password(inputPassword)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 영문(대문자,소문자), 특수문자(!,@,#,$,%,^,&,*,_,-), 숫자를 최소 1개 이상 조합한 8자 이상 18자 이하여야합니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호를 반환한다.")
+    void getValue() {
+        //given
+        Password password = new Password("apple12345!@");
+
+        //when
+        String actual = password.getValue();
+
+        //then
+        assertThat(actual).isEqualTo("apple12345!@");
+    }
+}
