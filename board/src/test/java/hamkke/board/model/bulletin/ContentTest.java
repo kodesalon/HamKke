0b998048fd1f82a033e44fc91064d1112dbde74c
cjh8746 <cjh87467@gmail.com>
2022-10-12 16:46:29 +0900

    test : content 테스트 코드 작성

---
+++ board/src/test/java/hamkke/board/model/bulletin/ContentTest.java
@@ -0,0 +1,36 @@
+package hamkke.board.model.bulletin;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ContentTest {
+
+    @ParameterizedTest
+    @DisplayName("글 내용은 최대 1000 자여야한다.")
+    @ValueSource(ints = {0,1001})
+    void validate(final int repeatCount) {
+        //given
+        String inputContent = "a".repeat(repeatCount);
+        //when, then
+        assertThatThrownBy(() -> new Content(inputContent)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("글 내용은 최소 1자 이상, 최대 1000자 입니다.");
+    }
+
+    @Test
+    @DisplayName("글 내용을 반환한다.")
+    void getValue() {
+        //given
+        Content content = new Content("테스트용 샘플 글 내용입니다.");
+
+        //when
+        String actual = content.getValue();
+
+        //then
+        assertThat(actual).isEqualTo("테스트용 샘플 글 내용입니다.");
+    }
+}
