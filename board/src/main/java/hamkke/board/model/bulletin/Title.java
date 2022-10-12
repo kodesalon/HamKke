1a1443ce42dc0949345d2a3957199830bd5dc601
cjh8746 <cjh87467@gmail.com>
2022-10-13 00:47:17 +0900

    refactor : 롬복을 활용한 이퀄즈 앤 해시코드 추가 및 변수 네이밍 변경

--- board/src/main/java/hamkke/board/model/bulletin/Title.java
+++ board/src/main/java/hamkke/board/model/bulletin/Title.java
@@ -1,38 +1,26 @@
 package hamkke.board.model.bulletin;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode
 public class Title {
 
     private static final int MAXIMUM_TITLE_SIZE = 25;
 
     private final String value;
 
-    public Title(final String inputTitle) {
-        validateSize(inputTitle);
-        value = inputTitle;
+    public Title(final String title) {
+        validateSize(title);
+        value = title;
     }
 
-    private void validateSize(final String inputTitle) {
-        if (inputTitle.isBlank() || inputTitle.length() > MAXIMUM_TITLE_SIZE) {
+    private void validateSize(final String title) {
+        if (title.isBlank() || title.length() > MAXIMUM_TITLE_SIZE) {
             throw new IllegalArgumentException("글 제목은 1글자 이상 25자 이하야합니다.");
         }
     }
 
     public String getValue() {
         return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Title title = (Title) o;
-        return Objects.equals(value, title.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }
