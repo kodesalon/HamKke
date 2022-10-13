5f5061c14def485ec06f55e4e3324173da57c52c
cjh8746 <cjh87467@gmail.com>
2022-10-13 11:35:39 +0900

    test : 테스트 코드 수정

--- board/src/test/java/hamkke/board/model/user/UserIdTest.java
+++ board/src/test/java/hamkke/board/model/user/UserIdTest.java
@@ -12,11 +12,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class UserIdTest {
 
     @ParameterizedTest
+    @DisplayName("유저 아이디를 입력받아 객체를 생성한다.")
+    @ValueSource(strings = {"apple1", "a1234567890123456789", "a12345b23"})
+    void create(final String input) {
+        //when
+        UserId userId = new UserId(input);
+
+        //then
+        assertThat(userId).isEqualTo(new UserId(input));
+    }
+
+    @ParameterizedTest
     @DisplayName("유저의 아이디는 공백 제외 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.")
     @ValueSource(strings = {"1apple", "_apple", " apple", "apple", "a12345678901234567890", "apple!", "@apple", "apple_hi", "", " "})
-    void validateUserId(final String inputUserId) {
+    void validateUserId(final String input) {
         //when, then
-        assertThatThrownBy(() -> new UserId(inputUserId)).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> new UserId(input)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유저의 아이디는 공백을 제외하고, 영문자(소문자)로 시작한 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.");
     }
 
