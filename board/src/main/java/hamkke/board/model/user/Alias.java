e2f566a6c5218b2d9903e92dee1b02e8eebc08c1
cjh8746 <cjh87467@gmail.com>
2022-10-13 11:36:43 +0900

    refactor : 롬복으로 이퀄즈 앤 해시코드 변경 변수 네이밍 변경 정규식 판별 로직 변경

--- board/src/main/java/hamkke/board/model/user/Alias.java
+++ board/src/main/java/hamkke/board/model/user/Alias.java
@@ -1,39 +1,29 @@
 package hamkke.board.model.user;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+
 import java.util.regex.Pattern;
 
+@EqualsAndHashCode
 public class Alias {
 
-    private static final String ALIAS_PATTERN = "^[a-z0-9가-힣]{1,8}$";
+    private static final String ALIAS_REGEX = "^[a-z0-9가-힣]{1,8}$";
+    private static final Pattern ALIAS_PATTERN = Pattern.compile(ALIAS_REGEX);
 
     private final String value;
 
-    public Alias(final String inputAlias) {
-        validateByAliasPattern(inputAlias);
-        value = inputAlias;
+    public Alias(final String alias) {
+        validateByAliasPattern(alias);
+        value = alias;
     }
 
-    private void validateByAliasPattern(final String inputAlias) {
-        if (!Pattern.matches(ALIAS_PATTERN, inputAlias)) {
+    private void validateByAliasPattern(final String alias) {
+        if (!ALIAS_PATTERN.matcher(alias).matches()) {
             throw new IllegalArgumentException("별칭은 특수문자와 영어(대문자)를 제외하여 8자 이하여야 합니다.");
         }
     }
 
     public String getValue() {
         return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Alias alias = (Alias) o;
-        return Objects.equals(value, alias.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }
