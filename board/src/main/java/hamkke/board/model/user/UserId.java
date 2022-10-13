410758d9e5e977a766e4bd9168b265d94a0437b0
cjh8746 <cjh87467@gmail.com>
2022-10-13 11:37:15 +0900

    refactor : 롬복으로 이퀄즈 앤 해시코드 변경 변수 네이밍 변경 정규식 판별 로직 변경

--- board/src/main/java/hamkke/board/model/user/UserId.java
+++ board/src/main/java/hamkke/board/model/user/UserId.java
@@ -1,39 +1,29 @@
 package hamkke.board.model.user;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+
 import java.util.regex.Pattern;
 
+@EqualsAndHashCode
 public class UserId {
 
-    public static final String USER_ID_PATTERN = "^[a-z]+[a-z 0-9]{5,19}$";
+    private static final String USER_ID_REGEX = "^[a-z]+[a-z 0-9]{5,19}$";
+    private static final Pattern USER_ID_PATTERN = Pattern.compile(USER_ID_REGEX);
 
     private final String value;
 
-    public UserId(final String inputUserId) {
-        validateByUserIdPattern(inputUserId);
-        value = inputUserId;
+    public UserId(final String userId) {
+        validateByUserIdPattern(userId);
+        value = userId;
     }
 
-    private void validateByUserIdPattern(final String inputUserId) {
-        if (!Pattern.matches(USER_ID_PATTERN, inputUserId)) {
+    private void validateByUserIdPattern(final String userId) {
+        if (!USER_ID_PATTERN.matcher(userId).matches()) {
             throw new IllegalArgumentException("유저의 아이디는 공백을 제외하고, 영문자(소문자)로 시작한 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.");
         }
     }
 
     public String getValue() {
         return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserId userId = (UserId) o;
-        return Objects.equals(value, userId.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }
