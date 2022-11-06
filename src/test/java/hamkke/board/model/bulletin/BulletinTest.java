ff02970f5bc6d6888a7f823d054b8fcec0d39e63
최지환 <cjh87467@gmail.com>
2022-11-06 15:43:38 +0900

    test : #3 테스트 코드 변경

--- src/test/java/hamkke/board/model/bulletin/BulletinTest.java
+++ src/test/java/hamkke/board/model/bulletin/BulletinTest.java
@@ -3,7 +3,6 @@ package hamkke.board.model.bulletin;
 import hamkke.board.model.bulletin.vo.Content;
 import hamkke.board.model.bulletin.vo.Title;
 import hamkke.board.model.user.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,28 +13,40 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class BulletinTest {
 
-    private Bulletin bulletin;
-    private User author;
+    private User createUser() {
+        final String loginId = "apple123";
+        final String password = "apple123!!";
+        final String alias = "삼다수";
+        final LocalDateTime createdDateTime = LocalDateTime.of(2022, 11, 5, 20, 5, 11);
+        return new User(loginId, password, alias, createdDateTime);
+    }
 
-    @BeforeEach
-    void setUp() {
+    private Bulletin createBulletin(final User author) {
         final String title = "sample title";
         final String content = "sample content";
         final LocalDateTime createdDateTime = LocalDateTime.of(2022, 11, 6, 12, 20);
-
-        author = new User("apple123", "apple12341", "별명", LocalDateTime.of(2022, 11, 6, 12, 20));
-        bulletin = new Bulletin(title, content, author, createdDateTime);
+        return new Bulletin(title, content, author, createdDateTime);
     }
 
     @Test
     @DisplayName("게시물 제목, 내용, 작성자, 작성 일시를 반환한다.")
     void getValues() {
+        //given
+        User user = createUser();
+        Bulletin bulletin = createBulletin(user);
+
+        // when
+        Title title = bulletin.getTitle();
+        Content content = bulletin.getContent();
+        User author = bulletin.getAuthor();
+        LocalDateTime createdDateTime = bulletin.getCreatedDateTime();
+
         //then
         assertAll(
-                () -> assertThat(bulletin.getTitle()).isEqualTo(new Title("sample title")),
-                () -> assertThat(bulletin.getContent()).isEqualTo(new Content("sample content")),
-                () -> assertThat(bulletin.getAuthor()).isEqualTo(author),
-                () -> assertThat(bulletin.getCreatedDateTime()).isEqualTo(LocalDateTime.of(2022, 11, 6, 12, 20)),
+                () -> assertThat(title).isEqualTo(new Title("sample title")),
+                () -> assertThat(content).isEqualTo(new Content("sample content")),
+                () -> assertThat(author).isEqualTo(user),
+                () -> assertThat(createdDateTime).isEqualTo(LocalDateTime.of(2022, 11, 6, 12, 20)),
                 () -> assertThat(bulletin.getLastModifiedDateTime()).isEqualTo(LocalDateTime.of(2022, 11, 6, 12, 20))
         );
     }
@@ -43,12 +54,16 @@ class BulletinTest {
     @Test
     @DisplayName("입력 받은 Author 의 게시물과 동일한지 비교한다. - 연관 관계 편의 메서드")
     void changeAuthor() {
+        //given
+        User user = createUser();
+        Bulletin bulletin = createBulletin(user);
+
         //when
-        boolean hasAuthor = bulletin.getAuthor()
+        boolean doesAuthorHasBulletin = bulletin.getAuthor()
                 .getBulletins()
                 .contains(bulletin);
 
         //then
-        assertThat(hasAuthor).isTrue();
+        assertThat(doesAuthorHasBulletin).isTrue();
     }
 }
--- src/test/java/hamkke/board/model/user/UserTest.java
+++ src/test/java/hamkke/board/model/user/UserTest.java
@@ -4,7 +4,6 @@ import hamkke.board.model.bulletin.Bulletin;
 import hamkke.board.model.user.vo.Alias;
 import hamkke.board.model.user.vo.LoginId;
 import hamkke.board.model.user.vo.Password;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,22 +14,19 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class UserTest {
 
-    private User user;
-
-    @BeforeEach
-    void setUp() {
+    private User createUser() {
         final String loginId = "apple123";
         final String password = "apple123!!";
         final String alias = "삼다수";
         final LocalDateTime createdDateTime = LocalDateTime.of(2022, 11, 5, 20, 5, 11);
-
-        user = new User(loginId, password, alias, createdDateTime);
+        return new User(loginId, password, alias, createdDateTime);
     }
 
     @Test
     @DisplayName("유저 아이디, 비밀번호, 별칭, 생성 일자를 반환한다.")
     void getValues() {
         //when
+        User user = createUser();
         LoginId actualLoginId = user.getLoginId();
         Password actualPassword = user.getPassword();
         Alias actualAlias = user.getAlias();
@@ -48,6 +44,7 @@ class UserTest {
     @DisplayName("게시물을 입력 받아 추가한다.")
     void addBulletin() {
         //given
+        User user = createUser();
         Bulletin bulletin = new Bulletin("제목", "본문 내용", user, LocalDateTime.now());
 
         //when
