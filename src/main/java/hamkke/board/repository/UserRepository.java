4f162dc5d1e4ba098c687a3076c6382630167632
최지환 <cjh87467@gmail.com>
2022-11-13 15:39:25 +0900

    feat : #20 메서드 추가 - loginId 와 Alias 로 User 를 찾을 수 있도록 메서드 추가

--- src/main/java/hamkke/board/repository/UserRepository.java
+++ src/main/java/hamkke/board/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package hamkke.board.repository;
 
 import hamkke.board.domain.user.User;
+import hamkke.board.domain.user.vo.Alias;
+import hamkke.board.domain.user.vo.LoginId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByLoginId(final LoginId loginId);
+
+    Optional<User> findUserByAlias(final Alias alias);
 }
