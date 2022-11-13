73f7437b9af2827a39c4b0e7dc5dfe0366dc7706
최지환 <cjh87467@gmail.com>
2022-11-13 15:38:36 +0900

    feat: #20 회원 가입 시, 예외처리 추가 - 이미 사용된 loginId 를 받으면 예외처리 반환 - 이미 사용된 Alias 를 받으면 예외처리 반환

--- src/main/java/hamkke/board/service/UserService.java
+++ src/main/java/hamkke/board/service/UserService.java
@@ -1,13 +1,17 @@
 package hamkke.board.service;
 
 import hamkke.board.domain.user.User;
+import hamkke.board.domain.user.vo.Alias;
+import hamkke.board.domain.user.vo.LoginId;
 import hamkke.board.repository.UserRepository;
 import hamkke.board.service.dto.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -17,8 +21,31 @@ public class UserService {
 
     @Transactional
     public Long join(final CreateUserRequest createUserRequest) {
+        validateDuplication(createUserRequest);
         User user = new User(createUserRequest.getLoginId(), createUserRequest.getPassword(), createUserRequest.getAlias());
         User savedMember = userRepository.save(user);
         return savedMember.getId();
+    }
+    private void validateDuplication(final CreateUserRequest createUserRequest) {
+        validateLoginIdDuplication(createUserRequest);
+
+        validateAliasDuplication(createUserRequest);
+    }
+    private void validateLoginIdDuplication(final CreateUserRequest createUserRequest) {
+        String loginId = createUserRequest.getLoginId();
+        userRepository.findUserByLoginId(new LoginId(loginId))
+                .ifPresent(user -> {
+                    log.debug("중복된 아이디 생성 loginId = {}", createUserRequest.getLoginId());
+                    throw new IllegalStateException("해당 ID는 이미 사용중 입니다.");
+                });
+    }
+
+    private void validateAliasDuplication(final CreateUserRequest createUserRequest) {
+        String alias = createUserRequest.getAlias();
+        userRepository.findUserByAlias(new Alias(alias))
+                .ifPresent(user -> {
+                    log.debug("중복된 별명 생성 loginId = {}", createUserRequest.getLoginId());
+                    throw new IllegalStateException("해당 별명은 이미 사용중 입니다.");
+                });
     }
 }
