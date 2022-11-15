bda7b0217b431c8431bcc39838abb39b76e18358
최지환 <cjh87467@gmail.com>
2022-11-15 23:21:52 +0900

    refactor : #20 Configuration 설정 분리

--- src/test/java/hamkke/board/controller/UserControllerTest.java
+++ src/test/java/hamkke/board/controller/UserControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,7 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
-@MockBean(JpaMetamodelMappingContext.class)
 class UserControllerTest {
 
     @Autowired
