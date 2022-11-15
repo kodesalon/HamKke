a611ad4fee4766e4cac1283f51d3469ae5ac1222
최지환 <cjh87467@gmail.com>
2022-11-15 22:58:58 +0900

    feat : #20 UserController 구현

---
+++ src/main/java/hamkke/board/controller/UserController.java
@@ -0,0 +1,29 @@
+package hamkke.board.controller;
+
+import hamkke.board.service.UserService;
+import hamkke.board.service.dto.CreateUserRequest;
+import hamkke.board.service.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/user")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity<UserResponse> join(@RequestBody final CreateUserRequest createUserRequest) {
+        log.info("join 요청 loginId = {}, password = {}, alias = {}", createUserRequest.getLoginId(), createUserRequest.getPassword(), createUserRequest.getAlias());
+        Long joinedUserId = userService.join(createUserRequest);
+        return ResponseEntity.ok(new UserResponse(joinedUserId));
+    }
+}
+
