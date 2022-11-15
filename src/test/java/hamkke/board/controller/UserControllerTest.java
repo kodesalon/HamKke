d32b5c6c92dfdf2f92c1c9ea970d0461d61ec0b9
최지환 <cjh87467@gmail.com>
2022-11-16 00:45:41 +0900

    test : #20 테스트 코드 변경 불필요한 의존 제거

--- src/test/java/hamkke/board/controller/UserControllerTest.java
+++ src/test/java/hamkke/board/controller/UserControllerTest.java
@@ -12,9 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
