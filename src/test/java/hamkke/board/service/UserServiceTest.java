783d1201a6e648d9d034f754e022578bd9e70070
최지환 <cjh87467@gmail.com>
2022-11-16 14:15:18 +0900

    test : #20 테스트 코드 변경

--- src/test/java/hamkke/board/service/UserServiceTest.java
+++ src/test/java/hamkke/board/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package hamkke.board.service;
 
+import hamkke.board.config.JpaAuditingConfiguration;
 import hamkke.board.repository.UserRepository;
 import hamkke.board.service.dto.CreateUserRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Import(JpaAuditingConfiguration.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserServiceTest {
