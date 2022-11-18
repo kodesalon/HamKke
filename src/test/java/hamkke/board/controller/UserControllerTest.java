3f9a79a9afb75f560aabb26dceac6d8c7d9d6987
최지환 <cjh87467@gmail.com>
2022-11-18 17:07:31 +0900

    test : #20 테스트 코드 작성

--- src/test/java/hamkke/board/controller/UserControllerTest.java
+++ src/test/java/hamkke/board/controller/UserControllerTest.java
@@ -12,7 +12,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -27,11 +30,16 @@ class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    private CreateUserRequest generateUserRequest() {
+        return new CreateUserRequest("apple123", "apple123!!", "아이폰");
+    }
+
     @Test
-    @DisplayName("loginId, password, alias 를 json 으로 전달받아 회원 가입을 하고, 201 상태 코드를 반환한다.")
+    @DisplayName("loginId, password, alias 를 json 으로 전달받아 회원 가입을 하고, HTTP 201 상태 코드를 반환한다.")
     void join() throws Exception {
         //given
-        CreateUserRequest createUserRequest = new CreateUserRequest("apple123", "apple123!!", "아이폰");
+        given(userService.join(any())).willReturn(1L);
+        CreateUserRequest createUserRequest = generateUserRequest();
 
         //when
         ResultActions actual = mockMvc.perform(
@@ -39,6 +47,49 @@ class UserControllerTest {
                         .content(objectMapper.writeValueAsString(createUserRequest)));
 
         //then
-        actual.andExpect(status().isCreated());
+        actual.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId").value("1"));
+    }
+
+    @Test
+    @DisplayName("회원가입 시 이미 존재하는 loginId 인 경우, 예외 코드를 담은 DTO 와 HTTP 400 상태코드를 반환한다.")
+    void joinFailedByLoginIdDuplication() throws Exception {
+        //given
+        given(userService.join(any())).willReturn(1L)
+                .willThrow(new IllegalStateException("해당 ID는 이미 사용중 입니다."));
+
+        CreateUserRequest existedUserRequest = generateUserRequest();
+        CreateUserRequest duplicationUserRequest = new CreateUserRequest("apple123", "banana123!", "마우스");
+        mockMvc.perform(post("/api/user/join").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(existedUserRequest)));
+
+        //when
+        ResultActions actual = mockMvc.perform(post("/api/user/join").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(duplicationUserRequest)));
+
+        //then
+        actual.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("해당 ID는 이미 사용중 입니다."));
+    }
+
+    @Test
+    @DisplayName("회원가입 시 이미 존재하는 alias 인 경우, 예외 코드를 담은 DTO 와 HTTP 400 상태코드를 반환한다.")
+    void joinFailedByAliasDuplication() throws Exception {
+        //given
+        given(userService.join(any())).willReturn(1L)
+                .willThrow(new IllegalStateException("해당 별명은 이미 사용중 입니다."));
+
+        CreateUserRequest existedUserRequest = generateUserRequest();
+        CreateUserRequest duplicationUserRequest = new CreateUserRequest("apple123", "apple123!!", "마우스");
+        mockMvc.perform(post("/api/user/join").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(existedUserRequest)));
+
+        //when
+        ResultActions actual = mockMvc.perform(post("/api/user/join").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(duplicationUserRequest)));
+
+        //then
+        actual.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("해당 별명은 이미 사용중 입니다."));
     }
 }
