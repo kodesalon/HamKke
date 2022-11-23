29875783bdf30fb55daa06df3bca5ccb3a6e4336
최지환 <cjh87467@gmail.com>
2022-11-23 20:06:15 +0900

    test : #20 test 변경
    단위 테스트로 변경

--- src/test/java/hamkke/board/service/UserServiceTest.java
+++ src/test/java/hamkke/board/service/UserServiceTest.java
@@ -1,57 +1,62 @@
 package hamkke.board.service;
 
-import hamkke.board.config.JpaAuditingConfiguration;
+import hamkke.board.domain.user.User;
+import hamkke.board.domain.user.vo.Alias;
+import hamkke.board.domain.user.vo.LoginId;
 import hamkke.board.repository.UserRepository;
 import hamkke.board.service.dto.CreateUserRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@Import(JpaAuditingConfiguration.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @Autowired
+    @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private User user;
+
+    @InjectMocks
     private UserService userService;
 
-    private CreateUserRequest generateCreateUserRequest() {
-        return new CreateUserRequest("apple123", "apple123!", "삼다수");
-    }
-
     @Test
-    @DisplayName("회원 가입 성공 시, 회원의 식별 id를 반환한다.")
+    @DisplayName("DTO 를 입력받아 회원가입을 한 후 userId를 반환한다.")
     void join() {
         //given
-        userService = new UserService(userRepository);
-        CreateUserRequest createUserRequest = generateCreateUserRequest();
+        given(user.getId()).willReturn(1L);
+        given(userRepository.save(any(User.class))).willReturn(user);
 
         //when
-        Long joinedId = userService.join(createUserRequest);
+        userService.join(new CreateUserRequest("apple123", "apple123!!", "삼다수"));
 
         //then
-        assertThat(joinedId).isInstanceOfAny(Long.class);
+        verify(userRepository, times(1)).findUserByAlias(any(Alias.class));
+        verify(userRepository, times(1)).findUserByLoginId(any(LoginId.class));
+        verify(userRepository, times(1)).save(any(User.class));
     }
 
     @Test
     @DisplayName("회원 가입 시, 중복된 loginId 를 입력받은 경우 예외를 반환한다.")
     void validateLoginIdDuplication() {
         //given
-        userService = new UserService(userRepository);
-        CreateUserRequest createUserRequest = generateCreateUserRequest();
-        CreateUserRequest duplicatedUserRequest =  new CreateUserRequest("apple123", "banana123!", "삼다수123");
-        userService.join(createUserRequest);
+        CreateUserRequest duplicatedCreateUserRequest = new CreateUserRequest("apple123", "apple123!!", "삼다수");
+        given(userRepository.findUserByLoginId(any(LoginId.class))).willReturn(Optional.of(user));
 
         //when, then
-        assertThatThrownBy(() -> userService.join(duplicatedUserRequest)).isInstanceOf(IllegalStateException.class)
+        assertThatThrownBy(() -> userService.join(duplicatedCreateUserRequest)).isInstanceOf(IllegalStateException.class)
                 .hasMessage("해당 ID는 이미 사용중 입니다.");
     }
 
@@ -59,13 +64,11 @@ class UserServiceTest {
     @DisplayName("회원 가입 시, 중복된 alias 를 입력받은 경우 예외를 반환한다.")
     void validateAliasDuplication() {
         //given
-        userService = new UserService(userRepository);
-        CreateUserRequest createUserRequest = generateCreateUserRequest();
-        CreateUserRequest duplicatedUserRequest =  new CreateUserRequest("banana123", "banana123!", "삼다수");
-        userService.join(createUserRequest);
+        CreateUserRequest duplicatedCreateUserRequest = new CreateUserRequest("apple123", "apple123!!", "삼다수");
+        given(userRepository.findUserByAlias(any(Alias.class))).willReturn(Optional.of(user));
 
         //when, then
-        assertThatThrownBy(() -> userService.join(duplicatedUserRequest)).isInstanceOf(IllegalStateException.class)
+        assertThatThrownBy(() -> userService.join(duplicatedCreateUserRequest)).isInstanceOf(IllegalStateException.class)
                 .hasMessage("해당 별명은 이미 사용중 입니다.");
     }
 }
