d97a34bcc53ecb8cc2d5e59e46d5faa6b55158ac
최지환 <cjh87467@gmail.com>
2022-11-24 20:11:04 +0900

    test : #3 테스트 코드 변경

--- src/test/java/hamkke/board/model/user/vo/LoginIdTest.java
+++ src/test/java/hamkke/board/model/user/vo/LoginIdTest.java
@@ -1,4 +1,4 @@
-package hamkke.board.model.user;
+package hamkke.board.model.user.vo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,17 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
-class UserIdTest {
+class LoginIdTest {
 
     @ParameterizedTest
     @DisplayName("유저 아이디를 입력받아 객체를 생성한다.")
     @ValueSource(strings = {"apple1", "a1234567890123456789", "a12345b23"})
     void create(final String input) {
         //when
-        UserId userId = new UserId(input);
+        LoginId loginId = new LoginId(input);
 
         //then
-        assertThat(userId).isEqualTo(new UserId(input));
+        assertThat(loginId).isEqualTo(new LoginId(input));
     }
 
     @ParameterizedTest
@@ -28,7 +28,7 @@ class UserIdTest {
     @ValueSource(strings = {"1apple", "_apple", " apple", "apple", "a12345678901234567890", "apple!", "@apple", "apple_hi", "", " "})
     void validateUserId(final String input) {
         //when, then
-        assertThatThrownBy(() -> new UserId(input)).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> new LoginId(input)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유저의 아이디는 공백을 제외하고, 영문자(소문자)로 시작한 영문자(소문자)와 숫자의 조합으로 6자 이상 20자 이하여야합니다.");
     }
 
@@ -36,10 +36,10 @@ class UserIdTest {
     @DisplayName("유저 아이디를 반환하다.")
     void getValue() {
         //given
-        UserId apple = new UserId("banana12");
+        LoginId loginId = new LoginId("banana12");
 
         //when
-        String actual = apple.getValue();
+        String actual = loginId.getValue();
 
         //then
         assertThat(actual).isEqualTo("banana12");
