5233ac8dd4cfbb7478dc34ce3fb7b5848878342d
최지환 <cjh87467@gmail.com>
2022-12-01 01:19:43 +0900

    refactor : #20 UserRepository 변경
    유니크 제약 조건 추가에 따라, 사용하지 않는 메서드 삭제

--- src/main/java/hamkke/board/repository/UserRepository.java
+++ src/main/java/hamkke/board/repository/UserRepository.java
@@ -1,15 +1,7 @@
 package hamkke.board.repository;
 
 import hamkke.board.domain.user.User;
-import hamkke.board.domain.user.vo.Alias;
-import hamkke.board.domain.user.vo.LoginId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findUserByLoginId(final LoginId loginId);
-
-    Optional<User> findUserByAlias(final Alias alias);
 }
