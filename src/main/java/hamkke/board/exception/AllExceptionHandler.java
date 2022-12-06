67a8eac02fcdfa3ed67fabd555969a019316d9fb
최지환 <cjh87467@gmail.com>
2022-12-06 15:21:07 +0900

    refactor : #20 presentation layer 에서의 검증로직 추가

--- src/main/java/hamkke/board/exception/AllExceptionHandler.java
+++ src/main/java/hamkke/board/exception/AllExceptionHandler.java
@@ -2,6 +2,7 @@ package hamkke.board.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AllExceptionHandler {
 
-    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorResponse> handleDuplicationException(final RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
