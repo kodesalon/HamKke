e9eecae5ebf77d7c57a7bc4d92236c8613ac5bc1
최지환 <cjh87467@gmail.com>
2022-12-07 21:49:00 +0900

    test : #34 테스트 코드 변경
    로그인 시 , loginId 와 password 가 일치하지 않는 경우
    로그인 시, 존재하지 않는 loginId 로 접근하는 경우에 대한 테스트 코드 작성
    및
    LoginResponse 추가에 따른 변경

--- src/test/java/hamkke/board/service/UserServiceTest.java
+++ src/test/java/hamkke/board/service/UserServiceTest.java
@@ -1,10 +1,14 @@
 package hamkke.board.service;
 
 import hamkke.board.domain.user.User;
+import hamkke.board.domain.user.vo.Alias;
 import hamkke.board.domain.user.vo.LoginId;
+import hamkke.board.domain.user.vo.Password;
 import hamkke.board.repository.UserRepository;
 import hamkke.board.service.dto.CreateUserRequest;
 import hamkke.board.service.dto.LoginRequest;
+import hamkke.board.service.dto.LoginResponse;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -85,18 +89,49 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("로그인 시, loginId 와 password 가 일치하면, 일치한 User 를 반환한다.")
+    @DisplayName("로그인 시, loginId 와 password 가 일치하면, loginResponse dto 를 반환한다.")
     void login() {
         //given
-        when(user.isInCollectPassword(any())).thenReturn(false);
+        when(user.getId()).thenReturn(1L);
+        when(user.getAlias()).thenReturn(new Alias("삼다수"));
+        when(user.isInCollectPassword(any(Password.class))).thenReturn(false);
+        when(userRepository.findUserByLoginId(any(LoginId.class))).thenReturn(Optional.of(user));
+
+        LoginRequest loginRequest = new LoginRequest("apple123", "apple123!!");
+        SoftAssertions softly = new SoftAssertions();
+
+        //when
+        LoginResponse loginResponse = userService.login(loginRequest);
+
+        //then
+        softly.assertThat(loginResponse.getUserId()).isEqualTo(1L);
+        softly.assertThat(loginResponse.getAlias()).isEqualTo("삼다수");
+        softly.assertAll();
+    }
+
+    @Test
+    @DisplayName("로그인 시, loginId 와 password 가 일치하지 않으면, 예외를 반환한다.")
+    void loginFailed() {
+        //given
+        when(user.isInCollectPassword(any(Password.class))).thenReturn(true);
         when(userRepository.findUserByLoginId(any(LoginId.class))).thenReturn(Optional.of(user));
 
         LoginRequest loginRequest = new LoginRequest("apple123", "apple123!!");
 
-        //when
-        User loginUser = userService.login(loginRequest);
+        //when, then
+        assertThatThrownBy(() -> userService.login(loginRequest)).isInstanceOf(IllegalStateException.class)
+                .hasMessage("비밀번호가 일치하지 않습니다.");
+    }
 
-        //then
-        assertThat(loginUser).isEqualTo(user);
+    @Test
+    @DisplayName("로그인 시, loginId 가 DB에 존재하지 않은 경우 예외를 반환한다.")
+    void loginFailedWhenNotExistLoginId() {
+        //given
+        when(userRepository.findUserByLoginId(any(LoginId.class))).thenThrow(new IllegalArgumentException("존재하지 않는 ID 입니다."));
+        LoginRequest loginRequest = new LoginRequest("banana123", "apple123!!");
+
+        //when
+        assertThatThrownBy(() -> userService.login(loginRequest)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 ID 입니다.");
     }
 }
