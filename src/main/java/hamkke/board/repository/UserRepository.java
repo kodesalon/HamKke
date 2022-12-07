eac0b8b36774173392af94aa92e52247c72548d1
최지환 <cjh87467@gmail.com>
2022-12-07 17:03:47 +0900

    refactor : #34 findUserByLoginId 메서드 추가

--- src/main/java/hamkke/board/repository/UserRepository.java
+++ src/main/java/hamkke/board/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package hamkke.board.repository;
 
 import hamkke.board.domain.user.User;
+import hamkke.board.domain.user.vo.LoginId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByLoginId(final LoginId loginId);
 }
