1e2530d91df246a7e658c94c0ca57623cc8fe3c2
최지환 <cjh87467@gmail.com>
2022-12-07 22:24:26 +0900

    refactor  : #34 IllegalArgumentException 핸들링 기능 추가

--- src/main/java/hamkke/board/exception/AllExceptionHandler.java
+++ src/main/java/hamkke/board/exception/AllExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AllExceptionHandler {
 
-    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class,IllegalArgumentException.class})
     public ResponseEntity<ErrorResponse> handleDuplicationException(final RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
