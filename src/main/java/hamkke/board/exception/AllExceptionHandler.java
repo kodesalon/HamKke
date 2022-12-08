cde62b62720148c4b2a12b41ddfeb622c5ee8eb5
최지환 <cjh87467@gmail.com>
2022-12-08 16:03:28 +0900

    refactor  : #34 메서드 이름 변경

--- src/main/java/hamkke/board/exception/AllExceptionHandler.java
+++ src/main/java/hamkke/board/exception/AllExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AllExceptionHandler {
 
     @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class,IllegalArgumentException.class})
-    public ResponseEntity<ErrorResponse> handleDuplicationException(final RuntimeException e) {
+    public ResponseEntity<ErrorResponse> handleBusinessException(final RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
     }
