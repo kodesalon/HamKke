6857bec7297c37c19261a06078b70d9bad1f00b1
최지환 <cjh87467@gmail.com>
2022-12-24 01:28:40 +0900

    refactor : #38 jwt 설정 변경 시크릿 키를 yml 설정 파일에서 관리할 수 있도록 변경 시크릿 키 암호를 byte 로 변환 후 key 를 생성 하도록 변경

--- src/main/java/hamkke/board/web/jwt/TokenResolver.java
+++ src/main/java/hamkke/board/web/jwt/TokenResolver.java
@@ -1,10 +1,10 @@
 package hamkke.board.web.jwt;
 
 import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.time.Duration;
 import java.util.Date;
@@ -13,42 +13,54 @@ import java.util.Date;
 public class TokenResolver {
 
     private static final String ISSUER = "hamkke";
-    private static final String SECRET_KEY = "password1234";
-    private static final int TOKEN_EXPIRED_TIME = 30;
-    private static final byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
-    private static final Key signatureKey= new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS256.getJcaName());
+    private final String secret;
+    private final Long tokenValidityInMilliseconds;
+
+    public TokenResolver(final @Value("${jwt.secret}") String secret,
+                         final @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
+
 
     public String createToken(final Long userId) {
         Date now = new Date();
-
+        generateKey();
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .claim("userId", userId)
                 .setIssuer(ISSUER)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(TOKEN_EXPIRED_TIME).toMillis()))
-                .claim("userId", userId)
-                .signWith(SignatureAlgorithm.HS256, signatureKey)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(tokenValidityInMilliseconds).toMillis()))
                 .compact();
     }
 
     public Long getUserId(final String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder()
+                .setSigningKey(generateKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .get("userId", Long.class);
     }
 
-    public boolean validateToken(String jwtToken) {
+    public boolean validateToken(final String token) {
         try {
-            Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
-                    .parseClaimsJws(jwtToken);
+            Jwts.parserBuilder().setSigningKey(generateKey()).build().parseClaimsJws(token);
             return true;
-        } catch (final SignatureException exception) {
+        } catch (final SignatureException | SecurityException | MalformedJwtException exception) {
             throw new JwtException("토큰이 유효하지 않습니다.");
         } catch (final ExpiredJwtException exception) {
             throw new JwtException("토큰의 유효기간이 만료되었습니다.");
+        } catch (final UnsupportedJwtException exception) {
+            throw new JwtException("지원하지 않는 토큰입니다.");
+        } catch (final IllegalArgumentException exception) {
+            throw new JwtException("JWT 토큰이 잘못되었습니다.");
         }
+    }
+
+    private Key generateKey() {
+        return new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS256.getJcaName());
     }
 }
