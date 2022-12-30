8da9e189464761cf9493403c6a27bdddd0fccf2a
최지환 <cjh87467@gmail.com>
2022-12-30 16:17:01 +0900

    refactor : #38 RefreshToken JPA 설정 추가

--- src/main/java/hamkke/board/web/jwt/RefreshToken.java
+++ src/main/java/hamkke/board/web/jwt/RefreshToken.java
@@ -2,24 +2,36 @@ package hamkke.board.web.jwt;
 
 import hamkke.board.domain.user.User;
 import io.jsonwebtoken.JwtException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RefreshToken {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
+
+    @Column(nullable = false, unique = true)
     private String token;
+
+    @Column(nullable = false)
     private LocalDateTime expirationTime;
 
     public RefreshToken(final User user, final String token, final LocalDateTime expirationTime) {
         this.user = user;
         this.token = token;
         this.expirationTime = expirationTime;
-    }
-
-    public String getToken() {
-        return token;
     }
 
     public void switchToken(final String newToken) {
