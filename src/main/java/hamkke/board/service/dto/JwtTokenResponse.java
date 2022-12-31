e8ec8ceb07f9c5c5c32985b68e9304d487b27527
최지환 <cjh87467@gmail.com>
2023-01-01 02:51:28 +0900

    refactor : #38 @JsonIgnore 추가

--- src/main/java/hamkke/board/service/dto/JwtTokenResponse.java
+++ src/main/java/hamkke/board/service/dto/JwtTokenResponse.java
@@ -1,11 +1,15 @@
 package hamkke.board.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 
 @Getter
 public class JwtTokenResponse {
 
+    @JsonIgnore
     private final String accessToken;
+
+    @JsonIgnore
     private final String refreshToken;
 
     public JwtTokenResponse(final String accessToken, final String refreshToken) {
