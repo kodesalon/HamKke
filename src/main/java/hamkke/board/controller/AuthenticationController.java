0d2edb7839ead38c7cece93ab2df4cc39cb4ab23
최지환 <cjh87467@gmail.com>
2023-01-01 01:38:45 +0900

    feat : #38  AuthenticationController 생성
     로그인 기능 추가

---
+++ src/main/java/hamkke/board/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package hamkke.board.controller;
+
+import hamkke.board.service.AuthenticationService;
+import hamkke.board.service.dto.LoginRequest;
+import hamkke.board.service.dto.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Validated @RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse = authenticationService.login(loginRequest);
+        return ResponseEntity.ok()
+                .header("Authorization", loginResponse.getAccessToken(), loginResponse.getRefreshToken())
+                .body(loginResponse);
+    }
+}
