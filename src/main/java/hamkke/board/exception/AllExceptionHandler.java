1ee59b0384d9da68232ea996412be308a3602627
최지환 <cjh87467@gmail.com>
2023-01-12 21:53:30 +0900

    refactor : #38  MethodArgumentNotValidException 처리 불가한 부분 변경 defaultMessage 처리 메서드 추가

--- src/main/java/hamkke/board/exception/AllExceptionHandler.java
+++ src/main/java/hamkke/board/exception/AllExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AllExceptionHandler {
 
+    private static final int INDEX_OF_ERROR = 0;
+
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<ErrorResponse> handleBusinessException(final RuntimeException e) {
         return ResponseEntity.internalServerError()
@@ -21,15 +23,22 @@ public class AllExceptionHandler {
                 .body(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler({IllegalArgumentException.class, MethodArgumentNotValidException.class, JwtException.class})
+    @ExceptionHandler({IllegalArgumentException.class, JwtException.class})
     public ResponseEntity<ErrorResponse> handleValidationException(final RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalAccessException(final RuntimeException e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentValidation(final MethodArgumentNotValidException e) {
         return ResponseEntity.badRequest()
-                .body(new ErrorResponse(e.getMessage()));
+                .body(new ErrorResponse(getDefaultMessage(e)));
+    }
+
+    private static String getDefaultMessage(final MethodArgumentNotValidException e) {
+        return e.getBindingResult()
+                .getAllErrors()
+                .get(INDEX_OF_ERROR)
+                .getDefaultMessage();
     }
 }
